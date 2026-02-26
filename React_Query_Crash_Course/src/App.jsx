170ee268183ca6fc8912c84854fc5e312d40438d
Md. Foysal Hossain <fh375170@gmail.com>
2026-02-26 18:40:47 +0600

    frist commit

---
+++ React_Query_Crash_Course/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductList from './component/ProductList'
+
+const App = () => {
+  return (
+    <>
+      <ProductList />
+    </>
+  )
+}
+
+export default App
---
+++ React_Query_Crash_Course/src/component/ProductList.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import axios from 'axios'
+import { useQuery } from '@tanstack/react-query'
+
+const retrieveProducts = async () => {
+    const response = await axios.get('http://localhost:3000/products');
+    return response.data;
+}
+
+const ProductList = () => {
+    const {data: products, error, isLoading} = useQuery({
+        queryKey: ['products'],
+        queryFn: retrieveProducts,
+    });
+
+    if(isLoading) return <div>Fetching Product...</div>
+    if(error) return <div>An error occurred: {error.message}</div>
+
+  return (
+    <div className="flex flex-col justify-center items-center w-3/5">
+      <h2 className="text-3xl my-2">Product List</h2>
+      <ul>
+        {
+          products&& products.map((product) => (
+            <li key={product.id}>
+                <img 
+                  className="w-48 h-48 object-cover rounded-sm"
+                src={product.thumbnail} 
+                alt={product.name}
+              />
+
+              <p className="text-3xl my-3">{product.title}</p>
+            </li>
+          ))}
+      </ul>
+    </div>
+  )
+}
+
+export default ProductList
